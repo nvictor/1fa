33faf4f54777d2775c3dbee7c69bf3ef93596a1a
Victor Noagbodji <noagbodjivictor@gmail.com>
2025-06-29 23:23:02 -0400

    Update parts.swift

--- parts.swift
+++ parts.swift
@@ -257,13 +257,6 @@ enum Part: String, CaseIterable, Codable {
     }
 }
 
-//
-//  Parts.entitlements
-//  Parts
-//
-//  Created by Victor Noagbodji on 5/4/25.
-//
-
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
